8fe89c189d9da0f3bc3c5299859f2f4f44fdf279
OH384 <olfa.hadhbaoui995@gmail.com>
2025-01-07 16:53:07 +0100

    update

--- src/test/java/com/e2etests/automation/page_objects/AuthenticationOrangehrmPage.java
+++ src/test/java/com/e2etests/automation/page_objects/AuthenticationOrangehrmPage.java
@@ -1,33 +1,31 @@
 package com.e2etests.automation.page_objects;
-
-import java.time.Duration;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import com.e2etests.automation.utils.ConfigFileReader;
+import com.e2etests.automation.utils.SelectFromListUtils;
 import com.e2etests.automation.utils.SeleniumUtils;
 import com.e2etests.automation.utils.Setup;
 import com.e2etests.automation.utils.Validations;
+import com.e2etests.automation.utils.Wait;
+
 
 public class AuthenticationOrangehrmPage {
 	private ConfigFileReader configFileReader;
 	private Validations validations;
 	private SeleniumUtils seleniumutils;
+	private Wait wait;
+	private SelectFromListUtils selectFromListUtils;
 	
 	final static String USER_NAME = "username";
 	final static String PASS_WORD = "password";
 	final static String LOGIN_BUTTON = "//button[@type='submit']";
-	final static String DASHBOARD_NOTE = "//h6[normalize-space()='Dashboard']";
-	final static String ACCOUNTMENU_BUTTON = "//i[@class='oxd-icon bi-caret-down-fill oxd-userdropdown-icon']";
-	final static String LOG_OUT = "//a[text()='Logout']";
-	final static String LOGIN_NOTE = "//h5[normalize-space()='Login']";
+	final static String ACCOUNTMENU_BUTTON = "//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[3]/ul/li/span";
+	final static String LOG_OUT = "//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[3]/ul/li/ul/li[4]/a";
+	final static String LOGIN_NOTE = "//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/h5";
+	final static String DASHBOARD_NOTE = "//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[1]/span/h6";
 	
 	@FindBy(how = How.NAME, using = USER_NAME )
 	public static WebElement userName;
@@ -37,10 +35,9 @@ public class AuthenticationOrangehrmPage {
 
 	@FindBy(how = How.XPATH, using = LOGIN_BUTTON)
 	public static WebElement loginButton;
-	
 	@FindBy(how = How.XPATH, using = DASHBOARD_NOTE)
 	public static WebElement dashboardNote;
-
+	
 	@FindBy(how = How.XPATH, using = ACCOUNTMENU_BUTTON)
 	public static WebElement accountMenuButton;
 
--- src/test/java/com/e2etests/automation/step_definitions/AuthenticationOrangehrmStepDefinition.java
+++ src/test/java/com/e2etests/automation/step_definitions/AuthenticationOrangehrmStepDefinition.java
@@ -1,13 +1,19 @@
 package com.e2etests.automation.step_definitions;
 
+import java.time.Duration;
+
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.e2etests.automation.page_objects.AuthenticationOrangehrmPage;
 import com.e2etests.automation.utils.BasePage;
 import com.e2etests.automation.utils.ConfigFileReader;
+import com.e2etests.automation.utils.SelectFromListUtils;
 import com.e2etests.automation.utils.SeleniumUtils;
+import com.e2etests.automation.utils.Setup;
 import com.e2etests.automation.utils.Validations;
+import com.e2etests.automation.utils.Wait;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -19,92 +25,72 @@ public class AuthenticationOrangehrmStepDefinition extends BasePage {
 	private Validations validations;
 	private ConfigFileReader configfilereader;
 	private SeleniumUtils seleniumutils;
+	private Wait wait;
+	private SelectFromListUtils selectFromListUtils;
 
 	public AuthenticationOrangehrmStepDefinition() {
 		this.authenticationOrangehrmPage = new AuthenticationOrangehrmPage();
 		this.validations = new Validations();
 		this.configfilereader = new ConfigFileReader();
 		this.seleniumutils = new SeleniumUtils();
+		this.wait = new Wait (driver);
+		this.selectFromListUtils = new SelectFromListUtils();
 	}
-//
-//	@Given("je suis sur la page de connexion d OrangeHRM")
-//	public void jeSuisSurLaPageDeConnexionDOrangeHRM(String url) {
-//		seleniumutils.get(configfilereader.getProperties(url));
-//	}
-//
-//	@When("je saisis un nom d utilisateur {string}")
-//	public void jeSaisisUnNomDUtilisateur(String username) {
-//		seleniumutils.writeText(authenticationOrangehrmPage.userName, configfilereader.getProperties("home.login"));
-//	}
-//
-//	@When("je saisis un mot de passe {string}")
-//	public void jeSaisisUnMotDePasse(String password) {
-//		seleniumutils.writeText(authenticationOrangehrmPage.passWord, configfilereader.getProperties("home.password"));
-//	}
-//
-//	@When("je clique sur le bouton de connexion")
-//	public void jeCliqueSurleBoutonDeConnexion() {
-//		seleniumutils.click(authenticationOrangehrmPage.loginButton);
-//
-//	}
-//
-//	@Then("je me redirige vers le tableau de bord d OrangeHRM")
-//	public void jeMeRedirigeVersLeTableauDeBordDOrangeHRM() {
-//		Assert.assertEquals("Redirection vers le tableau de bord échouée", authenticationOrangehrmPage.dashboardNote);
-//		driver.quit();
-//	}
-//	
-//	@When("je clique sur l'icône de propriétaire du compte")
-//    public void jeCliqueSurLIconeDeProprietaireDuCompte() {
-//		seleniumutils.click(authenticationOrangehrmPage.accountMenuButton);
-//	}
-//
-//	@When("je clique sur le bouton de déconnexion")
-//	public void jeCliqueSurLeBoutonDeDeconnexion() {
-//		seleniumutils.click(authenticationOrangehrmPage.logOut);
-//	}
-//	@Then("je suis redirigé vers la page de connexion")
-//    public void jeSuisRedirigeVersLaPageDeConnexion() {
-//		Assert.assertEquals("La page de connexion devrait être affichée", authenticationOrangehrmPage.loginNote);
-//		driver.quit();
-//	}
 
 	@Given("je suis sur la page de connexion d OrangeHRM {string}")
 	public void jeSuisSurLaPageDeConnexionDOrangeHRM(String url) {
 		seleniumutils.get(configfilereader.getProperties(url));
+		driver = Setup.getDriver();
 	}
+
 	@When("je saisis un nom d utilisateur {string}")
 	public void jeSaisisUnNomDUtilisateur(String userName) {
+		wait.forElementToBeDisplayed(Duration.ofSeconds(10), authenticationOrangehrmPage.userName, "Username field");
 		seleniumutils.writeText(authenticationOrangehrmPage.userName, configfilereader.getProperties("home.login"));
 	}
+
 	@When("je saisis un mot de passe  {string}")
 	public void jeSaisisUnMotDePasse(String passWord) {
 		seleniumutils.writeText(authenticationOrangehrmPage.passWord, configfilereader.getProperties("home.password"));
 	}
+
 	@When("je clique sur le bouton de connexion")
 	public void jeCliqueSurLeBoutonDeConnexion() {
 		seleniumutils.click(authenticationOrangehrmPage.loginButton);
 	}
+
+//	@Then("je me redirige vers le tableau de bord d OrangeHRM")
+//	public void jeMeRedirigeVersLeTableauDeBordDOrangeHRM() {
+//		Assert.assertEquals("Redirection vers le tableau de bord échouée",
+//				authenticationOrangehrmPage.accountMenuButton);
+//		driver.quit();
+//	}
+	
 	@Then("je me redirige vers le tableau de bord d OrangeHRM")
 	public void jeMeRedirigeVersLeTableauDeBordDOrangeHRM() {
-		Assert.assertEquals("Redirection vers le tableau de bord échouée", authenticationOrangehrmPage.dashboardNote);
+		wait.forElementToBeDisplayed(Duration.ofSeconds(30), authenticationOrangehrmPage.dashboardNote, "dashboardNote");
+		Assert.assertEquals("Dashboard",authenticationOrangehrmPage.dashboardNote.getText());
 		driver.quit();
 	}
-	
+
 	@When("je clique sur l icone de proprieteaire du compte")
-    public void jeCliqueSurLIconeDeProprietaireDuCompte() {
+	public void jeCliqueSurLIconeDeProprietaireDuCompte() {
+		wait.forElementToBeDisplayed(Duration.ofSeconds(30), authenticationOrangehrmPage.accountMenuButton, "accountMenuButton");
 		seleniumutils.click(authenticationOrangehrmPage.accountMenuButton);
+//		selectFromListUtils.selectDropDownListByVisibleText(authenticationOrangehrmPage.logOut,"Logout");
 	}
-	
+
 	@When("je clique sur le bouton de déconnexion")
 	public void jeCliqueSurLeBoutonDeDeconnexion() {
+		wait.forElementToBeDisplayed(Duration.ofSeconds(30), authenticationOrangehrmPage.logOut, "Logout");
 		seleniumutils.click(authenticationOrangehrmPage.logOut);
 	}
+
 	@Then("je me redirige vers la page de connexion")
-    public void jeSuisRedirigeVersLaPageDeConnexion() {
-		Assert.assertEquals("La page de connexion devrait être affichée", authenticationOrangehrmPage.loginNote);
+	public void jeSuisRedirigeVersLaPageDeConnexion() {
+		wait.forElementToBeDisplayed(Duration.ofSeconds(30), authenticationOrangehrmPage.loginNote, "loginNote");
+		Assert.assertEquals("Login",authenticationOrangehrmPage.loginNote.getText());
 		driver.quit();
-}
-
-
 	}
+
+}
